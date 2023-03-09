0bdd4eb518089ba4e98d7a4577ff9bb7c037fdf5
Eugen Borodin <Azsalatay@gmail.com>
2023-03-09 18:10:40 +0100

    Add Kirill first

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
   public static void main(String[] args) {
     Soda mySoda = new Soda("lemon");
-    mySoda.GetMyDrinkString();
-    System.out.println(mySoda.GetMyDrinkString()); //проверочный вывод
+    Soda firstMySoda = new Soda(null);
+    Soda secondMySoda = new Soda("");
+    System.out.println();
   }
 }
--- src/Soda.java
+++ src/Soda.java
@@ -1,10 +1,10 @@
 //Задача 1
 //Создайте класс Soda (для определения типа газированной воды).
 public class Soda {
-  private final String supplement;
+  private String supplement;
 
   //У класса должен быть конструктор,
-  public Soda (String supplement) {
+  public Soda(String supplement) {
     this.supplement = supplement;
   }
   // принимающий 1 аргумент при инициализации - строку добавка (например, "малина"),
@@ -12,9 +12,12 @@ public class Soda {
 
   //В этом классе реализуйте метод public String GetMyDrinkString(),
   public String GetMyDrinkString() {
-
-    return String.format("%s", supplement);
+    if (supplement != null && !supplement.isEmpty()) {
+      return "Газировка и " + supplement;
+    } else {
+      return "Обычная Газировка";
+      // Возвращающий строку Газировка и {добавка} в случае наличия добавки.
+      //Если добавки нет, нужно вернуть строку "Обычная газировка".
+    }
   }
-  // Возвращающий строку Газировка и {добавка} в случае наличия добавки.
-  //Если добавки нет, нужно вернуть строку "Обычная газировка".
 }
