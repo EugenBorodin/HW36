4104f33cece9634216694c387b0c0e5244a10571
Eugen Borodin <Azsalatay@gmail.com>
2023-03-09 19:26:46 +0100

    Add Kirill last

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
   public static void main(String[] args) {
-    Soda mySoda = new Soda("lemon");
+
     Soda firstMySoda = new Soda(null);
+    System.out.println(firstMySoda.getMyDrinkString());
     Soda secondMySoda = new Soda("");
-    System.out.println();
+    System.out.println(secondMySoda.getMyDrinkString());
+    Soda thirdMySoda = new Soda("lemon");
+    System.out.println(thirdMySoda.getMyDrinkString());
   }
 }
--- src/Soda.java
+++ src/Soda.java
@@ -11,13 +11,16 @@ public class Soda {
   // отвечающий за добавку к выбираемому лимонаду.
 
   //В этом классе реализуйте метод public String GetMyDrinkString(),
-  public String GetMyDrinkString() {
-    if (supplement != null && !supplement.isEmpty()) {
-      return "Газировка и " + supplement;
-    } else {
+  public String getMyDrinkString() {
+    if (supplement == null) {
+      return "Обычная Газировка";
+    }
+    if (supplement.isEmpty()) {
       return "Обычная Газировка";
       // Возвращающий строку Газировка и {добавка} в случае наличия добавки.
       //Если добавки нет, нужно вернуть строку "Обычная газировка".
     }
+    return "Газировка и " + supplement;
   }
 }
+
